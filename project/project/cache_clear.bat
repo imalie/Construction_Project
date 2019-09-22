263ad358552a430e885c4a04317503927e45a7bf
imalie <imaldewaas@gmail.com>
2019-09-22 18:02:51 +0530

    remove files

--- project/project/cache_clear.bat
+++
@@ -1,4 +0,0 @@
-php artisan cache:clear
-php artisan route:cache
-php artisan config:cache
-php artisan view:clear
--- project/project/resources/views/wel/users/profile.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-
-@section('content')
-    <h1>profile page</h1>
-        
-@endsection
